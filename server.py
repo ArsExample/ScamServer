b72474264246b34d6c5be785fa10c757b390fc01
Arseniy <arseny.ackimov@yandex.ru>
2021-11-24 23:21:05 +0300

    Lots of new commands added

--- server.py
+++ server.py
@@ -8,32 +8,57 @@ import time
 def me():
     returned_output = str(subprocess.check_output("whoami"))[2:-1]
     conn.send(returned_output.encode("utf-8"))
-
 def clickLeft():
     mouse.click("left")
-    conn.send("Sucess".encode("utf-8"))
+    conn.send("Successfully clicked left button".encode("utf-8"))
 def clickRight():
     mouse.click("right")
-    conn.send("Sucess".encode("utf-8"))
+    conn.send("Successfully clicked right button".encode("utf-8"))
 def shutdown():
-    conn.send("Trying to shutdown server".encode("utf-8"))
+    conn.send("Trying to shutdown server...".encode("utf-8"))
     os.system("shutdown -p")
 def typing(text):
     time.sleep(0.2)
     keyboard.write(text)
-    conn.send("Success".encode("utf-8"))
+    conn.send("Successfully typed your text".encode("utf-8"))
 def switchWindow():
     keyboard.press("alt")
     keyboard.press("tab")
     keyboard.release("alt")
     keyboard.release("tab")
-    conn.send("Success".encode("utf-8"))
+    conn.send("Successfully switched window".encode("utf-8"))
 def closeWindow():
     keyboard.press("alt")
     keyboard.press("f4")
     keyboard.release("alt")
     keyboard.release("f4")
-    conn.send("Success".encode("utf-8"))
+    conn.send("Successfully closed window".encode("utf-8"))
+def hideWindows():
+    keyboard.press("win")
+    keyboard.press("d")
+    keyboard.release("win")
+    keyboard.release("d")
+    conn.send("Successfully hid all windows".encode("utf-8"))
+def changeLanguage():
+    keyboard.press("shift")
+    keyboard.press("alt")
+    keyboard.release("shift")
+    keyboard.release("alt")
+    conn.send("Successfully changed language".encode("utf-8"))
+def Mystify():
+    conn.send("Starting Mystify".encode("utf-8"))
+    os.system("Mystify.scr -a")
+    conn.send("Successfully Mystified".encode("utf-8"))
+def watchingYou():
+    os.system("start cmd")
+    time.sleep(0.1)
+    keyboard.write("I am watching you...")
+    time.sleep(3.0)
+    keyboard.press("alt")
+    keyboard.press("f4")
+    keyboard.release("alt")
+    keyboard.release("f4")
+    conn.send("Successfully typed 'I am watching you...'".encode("utf-8"))
 
 sock = socket.socket()
 sock.bind(("", 2345))
@@ -43,7 +68,10 @@ while True:
     conn, address = sock.accept()
 
     while True:
-        data = str(conn.recv(1024))[2:-1]
+        try:
+            data = str(conn.recv(1024))[2:-1]
+        except ConnectionResetError:
+            break
         if not data:
             break
         if data == "me?" or data == "whoami" or data == "me":
@@ -58,9 +86,22 @@ while True:
             switchWindow()
         elif data == "close" or data == "closeWindow" or data == "clWin":
             closeWindow()
+        elif data == "lang" or data == "switchLang":
+            changeLanguage()
+        elif data == "Mystify" or data == "mystify" or data == "myst":
+            Mystify()
+        elif data == "hide" or data == "hideWindows" or data == "hidW":
+            hideWindows()
+        elif data == "iamwatchingyou" or data == "watching" or data == "cmdScreammer":
+            watchingYou()
         elif "type" in data:
             spisok = data.split("=")
-            typing(spisok[1])
+            try:
+                typing(spisok[1])
+            except IndexError:
+                conn.send("Error 2: Invalid syntax".encode("utf-8"))
+            except Exception:
+                conn.send("Unknown error".encode("utf-8"))
         else:
             conn.send("Error 1: Unknown command".encode("utf-8"))
 
